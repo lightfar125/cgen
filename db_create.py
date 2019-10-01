5a12ff34f7f33685780e965035bd5da8d996cf1e
Filipp G <8738425+filipp-g@users.noreply.github.com>
2019-10-01 15:18:39 -0400

    gather all numbers into db

---
+++ db_create.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('ontario649.db')
+print("Opened database successfully")
+
+conn.execute('''CREATE TABLE numbers(
+        date            TEXT    PRIMARY KEY NOT NULL,
+        no1             INT     NOT NULL,
+        no2             INT     NOT NULL,
+        no3             INT     NOT NULL,
+        no4             INT     NOT NULL,
+        no5             INT     NOT NULL,
+        no6             INT     NOT NULL,
+        bonus           INT     NOT NULL);''')
+
+print("Table created successfully")
+
+conn.close()
---
+++ scraper.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+years = ['2019','2018','2017','2016','2015','2014','2013']
+
+with open('2019-2013.csv', 'w') as outcsv:
+    writer = csv.writer(outcsv)
+
+    for year in years:
+        quote_page = f'https://www.lotteryleaf.com/on/lotto-649/{year}'
+        soup = BeautifulSoup(urlopen(quote_page).read(), 'html.parser')
+
+        rows = soup.find_all('tr')
+        for row in rows[1:]:
+            date = row.find('td', class_='win-nbr-date').contents[0]
+            numbers = row.find('ul', class_='nbr-grp').contents
+            out = date.replace(',', '')
+            for item in numbers:
+                out += f",{item.contents[-1].strip()}"
+            print(out)
